61efb98ea489241fb1fb6bfd0ef2fda88f580ad6
Dennis Chen <dennis.gemini@gmail.com>
2011-11-11 00:37:10 +0800

    add pe inst

--- asm/boot/test/kernel.s
+++ asm/boot/test/kernel.s
@@ -3,26 +3,50 @@
 start:
 	mov      %cs, %ax
 	mov      %ax, %ds
+	mov      %ax, %es
 	mov      %ax, %ss
 	mov      $top_of_stack, %ax
 	mov      %ax, %sp
 
-	mov      $0xb800, %ax
-	mov      %ax, %es
-	xor      %di, %di
-	mov      $(80 * 25), %cx
-
-	mov      $0x0F, %ah
-	mov      $'.', %al
-
-	cld
-	rep      stosw
-
 	mov      $msg_loader_started, %si
 	call     print
+
+	cli
+	lidtl    idt_desc
+	lgdtl    gdt_desc
+
+	#for 386
+	mov      %cr0, %eax
+	or       $1, %eax
+	mov      %eax, %cr0
+
+	#for 286
+#	smsw     %ax
+#	or       $1, %ax
+#	lmsw     %ax
+
+	ljmpl    $8, start32
+
+.code   32
+.balign 4
+start32:
+
 1:
+	hlt
 	jmp      1b
 
-msg_loader_started: .ascii "Hello, world!\r\n\0"
+.code 16
+msg_loader_started: .ascii "Hello, Real Mode!\r\n\0"
 stack: .space 1024
 top_of_stack:
+
+.balign 4
+gdt_desc:
+	.word	 
+	.long
+
+.balign 4
+idt_desc:
+	.word 0
+	.word 0, 0
+
